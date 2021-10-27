fbdbaa07c86e25128feb731d21e66b5762674d0a
Haylee Hunter <haylee_hunter@aol.com>
2021-10-26 23:44:47 -0500

    finished lab

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Films from "./components/Films";
+import SingleFilm from "./components/SingleFilm";
+import People from "./components/People";
+import SinglePerson from "./components/SinglePerson";
+
+export default function App() {
+  return (
+    <Router>
+      <div className="container">
+        <Navbar />
+        <Switch>
+          <Route exact path="/films">
+            <Films />
+          </Route>
+          <Route exact path="/films/:filmid">
+            <SingleFilm />
+          </Route>
+          <Route exact path="/people">
+            <People />
+          </Route>
+          <Route exact path="/people/:personid">
+            <SinglePerson />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div class="text-center">
+        <img
+          src="https://p.kindpng.com/picc/s/83-834491_official-studio-ghibli-logo-hd-png-download.png"
+          class="rounded"
+          alt="..."
+        />
+          </div>
+          <div class="card">
+  <div class="text-center card-body">
+                <h1>Studio Ghibli Routing Lab:</h1>
+                <h5>
+                This is a lab to demonstrate my React capabilities. It utilizes
+                conditional rendering, fetching data from REST API using a
+                useEffect hook, managing multiple array maps utilizing a
+                useState hook, and Bootstrap for styling.
+                  </h5>
+                    <h5>
+                Click either button to view the resources for Studio Ghibli and
+                ENJOY!
+                  </h5>
+                    <h5>
+                EMAIL @ HayleeHunter85@gmail.com
+                    </h5>
+  </div>
+</div>
+    </>
+  );
+};
+
+export default Home;
