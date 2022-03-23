b10b24fec8d5d5bf4f96f369510bc1a976cb0611
Zomievey <haylee_Hunter@aol.com>
2022-03-23 00:10:17 -0500

    added navbar image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,29 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="col">
-    <nav className="bar">
-      <ul className="nav justify-content-center">
-        <li id="nav-item" class="nav-item mb-3">
-          <Link className="nav-link active" to={"/"}>
-            Home
-          </Link>
-        </li>
-        <li id="nav-item"className="nav-item mb-3">
-          <Link class="nav-link" to={"/films"}>
-            View Films
-          </Link>
-        </li>
-        <li id="nav-item" className="nav-item mb-3">
-          <Link class="nav-link" to={"/people"}>
-            View People
-          </Link>
-        </li>
-      </ul>
-    </nav></div>
+      <nav className="bar">
+        <div className="nav-image"></div>
+        
+
+        <ul className="nav justify-content-center">
+          <li id="nav-item" class="nav-item mb-3">
+            <Link className="nav-link active" to={"/"}>
+              Home |
+            </Link>
+          </li>
+          <li id="nav-item" className="nav-item mb-3">
+            <Link class="nav-link" to={"/films"}>
+              View Films |
+            </Link>
+          </li>
+          <li id="nav-item" className="nav-item mb-3">
+            <Link class="nav-link" to={"/people"}>
+              View People |
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
